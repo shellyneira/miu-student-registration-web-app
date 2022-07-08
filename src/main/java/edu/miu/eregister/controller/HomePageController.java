0371929a0553ebf13c48d2f9a9ea152b25634b83
sneirabastos@miu.edu <sneirabastos@miu.edu>
2022-07-08 16:13:24 -0500

    Add home page controller

---
+++ src/main/java/edu/miu/eregister/controller/HomePageController.java
@@ -0,0 +1,13 @@
+package edu.miu.eregister.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping({"/", "/eregister", "/eregister/home"})
+    public String displayHomePage() {
+        return "home/index";
+    }
+}
