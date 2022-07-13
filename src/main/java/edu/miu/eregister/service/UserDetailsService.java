21fddf96085d8b08c8c44ea5887dcb5d9369d95e
sneirabastos@miu.edu <shellynbastos@gmail.com>
2022-07-13 11:18:30 -0500

    Add User service

---
+++ src/main/java/edu/miu/eregister/service/UserDetailsService.java
@@ -0,0 +1,21 @@
+package edu.miu.eregister.service;
+
+import edu.miu.eregister.model.User;
+import edu.miu.eregister.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
+        return user;
+    }
+}
