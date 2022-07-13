3e2e4d27954800615cfa5b1220a9f9e1b8a06cbf
sneirabastos@miu.edu <shellynbastos@gmail.com>
2022-07-13 11:18:44 -0500

    Add User repository

---
+++ src/main/java/edu/miu/eregister/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.miu.eregister.repository;
+
+import edu.miu.eregister.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
